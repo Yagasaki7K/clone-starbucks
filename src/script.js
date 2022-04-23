051da37c54e9852d8daf5f6803b0f0e6bf1a253c
Anderson Marlon <23272064+Yagasaki7K@users.noreply.github.com>
2022-04-23 16:33:24 -0300

    Added: Two Coffee Effect

---
+++ src/script.js
@@ -0,0 +1,22 @@
+function changeGreen() {
+    document.getElementById("coffee").src="assets/img1.png";
+    document.getElementById("extras").style.background = "#017143";
+    document.getElementById("destaque").style.color = "#017143";
+    document.getElementById("button").style.background = "#017143";
+
+}
+
+function changePink() {
+    document.getElementById("coffee").src="assets/img2.png";
+    document.getElementById("extras").style.background = "#ffc0cb";
+    document.getElementById("destaque").style.color = "#ffc0cb";
+    document.getElementById("button").style.background = "#ffc0cb";
+}
+
+function changePurple(){
+    document.getElementById("coffee").src="assets/img3.png";
+    document.getElementById("extras").style.background = "#b62a8b";
+    document.getElementById("destaque").style.color = "#b62a8b";
+    document.getElementById("button").style.background = "#b62a8b";
+
+}
